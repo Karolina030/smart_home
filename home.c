291465f290f7dd3ce3dbbe14b15e3cc6fcd362fc
Gregory Jack <kmatuszczyk@icloud.com>
2021-06-26 18:33:26 +0200

    with calls

---
+++ home.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <time.h>
+#include <po_hi_time.h>
+
+
+int readMotion(int movement_data, int *person_in_room)
+{
+	if(movement_data == 1)
+	{
+		printf("Wykryto ruchu w pokoju\n");
+		*person_in_room = 1;
+		return 1;
+	}
+	else
+	{
+		printf("Nie wykryto postaci na zapisie wideo\n");
+		*person_in_room = 0;
+		return 0;		
+	}
+}
+
+int Awake_Processing_Rules(int movement_data, int *coffee_out)
+{
+	if(movement_data == 1)
+	{
+		printf("Wykryto obudzenie :)\n");
+		*coffee_out = 1;
+		return 1;
+	}
+	else
+	{
+		printf("Nie wykryto obudzenia\n");
+		*coffee_out = 0;
+		return 0;		
+	}
+}
